9376142f1c979832b2f9824e1a827eba5811626e
Saurlax <saurlax@qq.com>
2024-12-26 13:49:35 +0800

    feat: add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -4,9 +4,12 @@ import remarkMath from "remark-math";
 import pagefind from "astro-pagefind";
 import unocss from "unocss/astro";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [unocss({ injectReset: true }), pagefind()],
+  site: "https://saurlax.com",
+  integrations: [unocss({ injectReset: true }), pagefind(), sitemap()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [[rehypeKatex, { throwOnError: false, strict: false }]],
