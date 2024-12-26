580406f7cdba36e586e60410bd797c2eec1b6244
Saurlax <saurlax@qq.com>
2024-12-26 16:46:11 +0800

    feat: add rss

---
+++ src/pages/feed.xml.ts
@@ -0,0 +1,21 @@
+import rss from "@astrojs/rss";
+import type { APIContext } from "astro";
+import { getCollection } from "astro:content";
+
+export async function GET(context: APIContext) {
+  if (!context.site) {
+    return;
+  }
+  const blog = await getCollection("blog");
+  return rss({
+    title: "Saurlax's Blog",
+    description: "A blog about web development, programming, and more.",
+    site: context.site,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.date,
+      description: post.body.slice(0, 200),
+      link: `/blog/${post.slug}/`,
+    })),
+  });
+}
