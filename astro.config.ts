587496ada8fd064beeac66654dce57a32e04fd00
Saurlax <saurlax@qq.com>
2025-01-03 18:46:12 +0800

    feat: last update time

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,21 @@
 import { defineConfig } from "astro/config";
-import { rehypeShiki } from "@astrojs/markdown-remark";
-import rehypeKatex from "rehype-katex";
-import remarkMath from "remark-math";
-import rehypeMermaid from "rehype-mermaid";
+
 import pagefind from "astro-pagefind";
 import unocss from "unocss/astro";
 import sitemap from "@astrojs/sitemap";
+
+import { rehypeShiki } from "@astrojs/markdown-remark";
+import remarkMath from "remark-math";
+import remarkLastUpdated from "./plugins/remarkLastUpdated";
+import rehypeKatex from "rehype-katex";
+import rehypeMermaid from "rehype-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://saurlax.com",
   integrations: [unocss({ injectReset: true }), pagefind(), sitemap()],
   markdown: {
-    remarkPlugins: [remarkMath],
+    remarkPlugins: [remarkLastUpdated, remarkMath],
     rehypePlugins: [
       rehypeMermaid,
       rehypeShiki,
