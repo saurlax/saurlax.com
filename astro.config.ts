5376c809ed48e2027ed20935c2c546099319442c
Saurlax <saurlax@qq.com>
2025-02-09 10:38:42 +0800

    chore: remove mermaid renderer

--- astro.config.ts
+++ astro.config.ts
@@ -4,10 +4,8 @@ import pagefind from "astro-pagefind";
 import unocss from "unocss/astro";
 import sitemap from "@astrojs/sitemap";
 
-import { rehypeShiki } from "@astrojs/markdown-remark";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
-import rehypeMermaid from "rehype-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,11 +13,6 @@ export default defineConfig({
   integrations: [unocss({ injectReset: true }), pagefind(), sitemap()],
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [
-      rehypeMermaid,
-      rehypeShiki,
-      [rehypeKatex, { throwOnError: false, strict: false }],
-    ],
-    syntaxHighlight: false,
+    rehypePlugins: [[rehypeKatex, { throwOnError: false, strict: false }]],
   },
 });
